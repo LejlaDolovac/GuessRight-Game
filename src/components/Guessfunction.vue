2fee01365673c24c0e8b0c94e734101d0f83f1bd
Emil Petersson <emilpet93@gmail.com>
2018-12-11 09:20:36 +0100

    Basic function to guess random number

--- src/components/Guessfunction.vue
+++ src/components/Guessfunction.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="brain">
-        <p> Guess the number homie </p>
-        <input type='number'  v-model="number">
-        <button @click="testNumber()" > Submit Number</button>
-
-        <span>
-            {{ randomNumber() }}
-        </span>
+        <h3> Guess the number homie </h3>
+        <p> {{ message }} </p>
+        <p v-show="hideNum"> {{ number }} </p>
+        <input type="number" v-model="guessedNumber">
+        <button @click="guessNumber">Submit Number</button>
+        <br>
+        <br>
         <p class="counte_display">
             {{ count }}
         </p>
@@ -21,21 +21,29 @@
 <script>
 export default {
     name: 'Guessfunction',
+    data() {
+      return {
+        number: Math.floor(Math.random() * (10 - 1 + 1)) + 1,
+        guessedNumber: 0,
+        message: '',
+        hideNum: false
+      }
+    },
     computed: {
-        data() {
-            return {
-                number: 0
-            }
-            
-        },
-        count() {
-            return this.$store.state.count;
-        }
+      count() {
+        return this.$store.state.count;
+      }
     },
     methods: {
-        randomNumber: function () {
-            return Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-
+        guessNumber: function () {
+          if (this.number == this.guessedNumber) {
+              this.message = "Correct!"; 
+              this.hideNum = !this.hideNum;
+          } else if (this.number > this.guessedNumber) {
+              this.message = "The number is higher!";
+          } else if (this.number < this.guessedNumber) {
+              this.message = "The number is lower!";
+          }
         },
         increment() {
             this.$store.commit('increment')
