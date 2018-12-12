75d9db3d4d92430765cabf917335659a16a90532
Emil Petersson <emilpet93@gmail.com>
2018-12-12 11:03:34 +0100

    Solved conflict

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
     <Guessfunction/>
-    <Countdown/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Guessfunction from '@/components/Guessfunction.vue'
-import Countdown from '@/components/Countdown.vue'
 
 export default {
   name: 'home',
   components: {
-    Guessfunction,
-    Countdown
+    Guessfunction
   }
 }
 </script>
