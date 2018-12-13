c5bcab9fb3827963a722ba100779b4ded0e6efde
Victor <victor@vrmobil.se>
2018-12-13 11:30:38 +0100

    fuse

--- src/components/Guessfunction.vue
+++ src/components/Guessfunction.vue
@@ -1,38 +1,19 @@
 <template>
 <div class="brain">
         <h3> Guess the number homie </h3>
-<<<<<<< HEAD
-        <p> {{ message }} </p>
-        <p v-show="hideNum"> {{ number }} </p>
-        <input type="number" v-model="guessedNumber">
-        <button @click="guessNumber">Submit Number</button>
-        <br>
-        <br>
-        <p class="counte_display">
-            {{ count }}
-        </p>
-        <div class="counter_buttons">
-            <button class="button_counter_increment" @click="increment">Plus</button>
-            <button class="button_counter_decrement" @click="decrement">Minus</button>
-            <button class="button_counter_testing" @click="testingNumbers">Test</button>
-        </div>
-=======
         <button @click="timerFunction(); startShow = false; inputDisabled = false; timer = 10" v-show="startShow">Start</button>
         <p>Time left: <span>{{ timer }}</span></p>
         <p> {{ message }} </p>
         <p v-show="hideNum"> {{ this.$store.state.number }} </p>
         <div>
 
-        <input  class="search" type="number" v-model="guessedNumber" @keyup.enter="guessNumber">
+        <input  class="search" type="number" v-model="guessedNumber" @keyup.enter="guessNumber" :disabled="inputDisabled">
         </div>
         <button class="btn" @click="guessNumber">Press</button>
-            <input type="number" v-model="guessedNumber" @keyup.enter="guessNumber" :disabled="inputDisabled">
 
-        <button @click="guessNumber">Submit Number</button>
         <br>
         <br>
         <p>Your result is: <span>{{ this.$store.state.correctAnswers }}</span> </p>
->>>>>>> 395547381326acc68a839905a8c156e107068d21
     </div>
 </template>
 
@@ -41,36 +22,7 @@ export default {
     name: 'Guessfunction',
     data() {
       return {
-<<<<<<< HEAD
-        number: Math.floor(Math.random() * (10 - 1 + 1)) + 1,
-        guessedNumber: 0,
-        message: '',
-        hideNum: false
-      }
-    },
-    computed: {
-      count() {
-        return this.$store.state.count;
-      }
-    },
-    methods: {
-        guessNumber: function () {
-          if (this.number == this.guessedNumber) {
-              this.message = "Correct!";
-              this.hideNum = !this.hideNum;
-          } else if (this.number > this.guessedNumber) {
-              this.message = "The number is higher!";
-          } else if (this.number < this.guessedNumber) {
-              this.message = "The number is lower!";
-          }
-        },
-        increment() {
-            this.$store.commit('increment')
-        },
-        decrement() {
-            this.$store.commit('decrement')
-=======
-        guessedNumber: Number,
+        guessedNumber: '',
         message: '',
         hideNum: false,
         rightAnswers: 0,
@@ -86,10 +38,10 @@ export default {
     methods: {
         guessNumber: function () {
           if (this.$store.state.number == this.guessedNumber) {
-              this.message = "Correct!"; 
+              this.message = "Correct!";
               this.rightAnswers++;
               console.log(this.rightAnswers);
-              this.message = "Correct, my man!"; 
+              this.message = "Correct, my man!";
               this.hideNum = !this.hideNum;
               this.$store.state.correctAnswers++;
               console.log(this.correctAnswers);
@@ -99,7 +51,7 @@ export default {
                 this.hideNum = false
                 this.$store.commit('newRandomNumber')
                 this.message = '';
-                this.guessedNumber = Number;
+                this.guessedNumber = '';
                 this.inputDisabled = true
                 this.timer = 10
                 this.startShow = true
@@ -110,22 +62,7 @@ export default {
           } else if (this.$store.state.number < this.guessedNumber) {
               this.message = "The number is lower!";
           }
-          
-          }
->>>>>>> 395547381326acc68a839905a8c156e107068d21
-        },
-<<<<<<< HEAD
-        testingNumbers() {
-            if(this.$store.state.number == this.count) {
-                alert("Whaaaa!")
-            } else {
-                alert("Whööööö!")
-            }
-        }
-    }
-}
-=======
-
+          },
         timerFunction() {
             this.timerInterval = setInterval(() => {
                 this.timer--
@@ -139,17 +76,43 @@ export default {
                 }
               }, 1000);
         }
+    },
     }
-
->>>>>>> 9560afcc4f47413b6c5f9f914952d8fcf7b85af5
 </script>
 
 <style scoped>
-
 p{
     color: midnightblue;
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+.search{
+	width: 150px;
+	height: 17px;
+	-webkit-transition: .3s ease-in-out;
+	transition: .3s ease-in-out;
+    z-index: 10;
+    border-radius: 50px;
+}
+.search:hover {
+	box-shadow: 0px 0px 150px grey;
+    z-index: 2;
+    -webkit-transition: all 200ms ease-in;
+    -webkit-transform: scale(1.5);
+    -ms-transition: all 200ms ease-in;
+    -ms-transform: scale(1.5);
+    -moz-transition: all 200ms ease-in;
+    -moz-transform: scale(1.5);
+    transition: all 200ms ease-in;
+    transform: scale(1.8);
+}
+.btn{
+    margin-top: 20px;
+    border-radius: 200%;
+    height: 100px;
+    width: 100px;
+    color: black;
+    background-color: red;
 
 }
-
+.btn:focus {outline:0;}
 </style>
