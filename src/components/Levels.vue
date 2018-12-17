e00e39124d501beb4f34c7702daca5326cc138e9
lejla <llleyla_@hotmail.com>
2018-12-17 10:19:27 +0100

    level updated second time

--- src/components/Levels.vue
+++ src/components/Levels.vue
@@ -20,7 +20,7 @@
 
 <script>
 export default {
-    name: 'Lobby',
+    name: 'Levels',
     data() {
       return {
         numbers: [],
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,7 @@ export default {
   name: 'home',
   components: {
     Lobby
+    
   }
 }
 </script>
