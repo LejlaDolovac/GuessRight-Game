c8a49728e32e38c767302b684f312530905e47f6
Victor <victor@vrmobil.se>
2018-12-21 12:09:56 +0100

    component Login

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- fontawesome-->
+  <div class="navbar has-text-white has-background-black">
+    <div class="fontawesome-container">
+      <span class="is-size-5">Login with:</span>
+      <br>
+      <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }" class="fontawesome"/> Facebook <br>
+      <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'google'  }" class="fontawesome"/> Google
+    </div>
+  </div>
+</template>
+
+<script>
+import facebookLogin from 'facebook-login-vuejs';
+
+
+export default( {
+    name: 'Login'
+});
+</script>
+
+<style scoped>
+
+.fontawesome-container {
+  position: absolute;
+  right: 0;
+  }
+</style>
