4962d524cf970fafaa4652d650ebe94e6c0668c4
Victor <victor@vrmobil.se>
2018-12-21 13:45:51 +0100

    added login facebook

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,7 +4,7 @@
     <div class="fontawesome-container">
       <span class="is-size-5">Login with:</span>
       <br>
-      <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }" class="fontawesome"/> Facebook <br>
+      <a @click="facebookLogin"><font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }" class="fontawesome"/>Facebook </a> <br>
       <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'google'  }" class="fontawesome"/> Google
     </div>
   </div>
@@ -12,14 +12,38 @@
 
 <script>
 import facebookLogin from 'facebook-login-vuejs';
-
+import firebase from 'firebase'
 
 export default( {
-    name: 'Login'
+    name: 'Login',
+    methods: {
+      facebookLogin(){
+        var provider = new firebase.auth.FacebookAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+      }
+    }
 });
 </script>
 
 <style scoped>
+a {
+  color: #fff;
+}
 
 .fontawesome-container {
   position: absolute;
