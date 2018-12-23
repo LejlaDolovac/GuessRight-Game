0accd6b37d56d689be4accde2f7937a9a66c6056
Emil Petersson <emilpet93@gmail.com>
2018-12-23 14:44:49 +0100

    Import user profile data

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,34 +5,45 @@
       <br>
       <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }" class="fontawesome"/> Facebook <br>
       <a @click="googleLogin"><font-awesome-icon :icon="{ prefix: 'fab', iconName: 'google'  }" class="fontawesome"/> Google</a>
+      <span> {{ userName }} </span>
     </div>
   </div>
 </template>
 
 <script>
 import firebase from 'firebase';
+import {fb} from '../firebase-config'
 
 export default( {
     name: 'Login',
     data() {
       return {
-        user: ''
+        email: '',
+        userName: ''
       }
     },
     methods: {
       googleLogin() {
         const provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithRedirect(provider).then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        this.user = result.user;
-        }).catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var email = error.email;
-        var credential = error.credential;
-        })
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          }).catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+          })
+          var user = firebase.auth().currentUser;
+          if (user != null) {
+            user.providerData.forEach(function (profile) {
+            console.log(profile.displayName);
+            alert(profile.displayName);
+            console.log("  Email: " + profile.email);
+            console.log("  Photo URL: " + profile.photoURL);
+          })
+        }
       },
       logout() {
           firebase.auth().signOut().then(function() {
