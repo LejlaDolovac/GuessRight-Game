e48b0297968bc196d77b96fa9e274764bd96a6b5
Åsa Palholmen <asa@palholmen.se>
2019-01-03 22:32:05 +0100

    cleaned up

--- src/views/HighScore.vue
+++ src/views/HighScore.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="highscore">
     <HighScoreFunction/>
   </div>
 </template>
@@ -10,29 +10,12 @@ import HighScoreFunction from '@/components/HighScoreFunction.vue'
 
 export default {
   name: 'highscore',
-  firebase: {
-    numbers: db.ref('numbers')
-  },
+
   components: {
     HighScoreFunction
   }
 }
 </script>
 
-
 <style scoped>
-h1 {
-  font-size: 4em;
-  font-family: 'Black Ops One', cursive;
-  color: black;
-}
-
-.highscore {
-  background-image: linear-gradient(to bottom right, yellow, #e01171, #ab0e86, #59057b, #0f0766);
-  height: 1000px;
-}
-
-export default {
-  name: 'highScore',
-  
-}
+</style>
