86664ddd5824d4f6fbc415b251c4d26fb8a59c6c
Victor <victor@vrmobil.se>
2019-01-04 11:48:30 +0100

    Put some makeup on the rules

--- src/components/Rules.vue
+++ src/components/Rules.vue
@@ -6,12 +6,16 @@
         <div class="modal-content is-size-5 has-background-black has-text-white">
           <h1 class="title is-2 has-text-warning">Game Rules</h1>
             <ul>
-              <li>It is a turned based game.</li>
-              <li>The Player starts to guess a number between 1-10.</li>
-              <li>The turn then goes to the next AI-bots. First to guess the right number scores a point.</li>
-              <li>A game where the player guesses a number and competes against AI.</li>
+              <li>This is a turned based game, Player vs AI.</li>
+              <!-- <li>A game where the player guesses a number and competes against AI.</li> -->
+              <li>The Player selects a Level:</li>
+              <li><span class="has-text-dark is-size-4">Easy 1-10</span> or<span class="has-text-success is-size-4"> Medium 1-30</span> or <span class="has-text-light is-size-4">Hard 1-50</span></li>
+              <li>A countdown starts the game Ready,Set,Go!</li>
+              <li>The Player starts to type in any number between the interval of numbers for the chosen level.</li>
+              <li>The turn then goes to the next AI-bot. First to guess the right number scores a point.</li>
+              <li>Beat the BOT in best of 5 turns to earn your name on the scoreboard!</li>
               <li>First to guess the right number gets one point. Get as many points as you can within the countdown.</li>
-              <li>Compete for the #1 rank at the highscore board.</li>
+              <li>Compete for the #1 rank at the highscore board against other players.</li>
             </ul>
           <br>
           <button class="button is-medium is-primary" @click="close">Oki-doki!</button>
@@ -44,11 +48,15 @@ export default {
     overflow-x: hidden;
     margin: 3px;
 }
+h1 {
+  text-decoration: underline;
+  /* line-height: 2px; */
+}
 li {
     margin: 1.5%;
 }
 .container {
-  width: 50%;
+  width: auto;
   height: auto;
   background-color: red; /* For browsers that do not support gradients */
   background-image: linear-gradient(to right, #FF03A4 , #FF407E , #FF755F, #FFA64C, #FFD150, #F9F871); /* Standard syntax (must be last) */
